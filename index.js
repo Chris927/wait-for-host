0713053e58d11deb2fda4a964ff6f86f5315ecad
Chris Oloff <christian.oloff@gmail.com>
2015-01-01 13:19:42 +0200

    added spec for not-yet-open port, wip
    Quite ugly how we deal with time now...

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var net = require('net');
+
 module.exports = function(host, port, cb) {
-  cb(null);
+  var retriesRemaining = 10; // TODO: unused, options
+  var socket = null;
+  function tryToConnect() {
+    if (socket) socket.destroy();
+    console.log('trying to connect...');
+    var socket = net.createConnection(port, host, function(err) {
+      console.log('err', err);
+      socket.destroy();
+      cb(null);
+    });
+    socket.setTimeout(10); // TODO: options
+    socket.on('timeout', tryToConnect)
+    socket.on('error', tryToConnect);
+  }
+  tryToConnect();
 };
--- test/happy_spec.js
+++ test/happy_spec.js
@@ -4,9 +4,41 @@ var waitForPort = require('../index'),
 var should = require('should');
 
 describe('waitForPort', function() {
-  var host = 'localhost', port = 34887;
+  function tryToConnect(host, port, cb) {
+    var client = net.createConnection(port, host, function() {
+      console.log('connected!');
+      client.end();
+      cb(null);
+    });
+  }
+
+  describe('when port is not open', function() {
+    var host = 'localhost', port = 34888; // TODO: using different port number than below, as async specs may overlap
+    var server = null;
+    afterEach(function(done) {
+      server.close(done);
+    });
+    it('calls us back', function(done) {
+      var portIsReady = false;
+      waitForPort(host, port, function(err) {
+        portIsReady.should.be.true;
+        done();
+      });
+      setTimeout(function() { // wait a little
+        server = net.createServer();
+        server.listen(port, function() {
+          console.log('server is now listening...');
+          portIsReady = true;
+        });
+      }, 100);
+    });
+  });
+
   describe('when port is open already', function() {
+
+    var host = 'localhost', port = 34887;
     var server;
+
     beforeEach(function(done) {
       server = net.createServer();
       server.listen(port, function() {
@@ -27,11 +59,8 @@ describe('waitForPort', function() {
         (err === null).should.be.true;
 
         // can we connect?
-        var client = net.createConnection(port, host, function() {
-          console.log('connected!');
-          client.end();
-          done();
-        });
+        tryToConnect(host, port, done);
+
       });
     });
   });
